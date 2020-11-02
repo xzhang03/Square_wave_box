18541aceb6c489a7485d51c5b796272359194e2e
Stephen Zhang <x.stephen.zhang@gmail.com>
2020-11-02 18:04:53 -0500

    updates

--- square_waves_trinket/square_waves_trinket.ino
+++ square_waves_trinket/square_waves_trinket.ino
@@ -1,7 +1,7 @@
 //include the fast I/O 2 functions 
 // #include "DIO2.h"  
 
-const float modifier = 1.027;
+const float modifier = 1.0;
 const byte led_pin_1 = 2;
 const byte led_pin_2 = 3;
 const int pulsewidth = 6000 * modifier; // in micro secs
